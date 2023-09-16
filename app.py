e9499a596577ef1cd4235315bc4054088cfe2bd4
Waldemar Liedle <w.liedle@reply.de>
2023-09-16 17:37:00 +0200

    added endpoint /ocr/image_to_text

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
-# app.py
 import os
+import keras_ocr
+import matplotlib.pyplot as plt
 from flask import Flask, request, jsonify
-#import keras_ocr
-#import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-#pipeline = keras_ocr.pipeline.Pipeline()
-
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-]
-
-def _find_next_id():
-    return max(country["id"] for country in countries) + 1
-
-@app.get("/countries")
-def get_countries():
-    return jsonify(countries)
+pipeline = keras_ocr.pipeline.Pipeline()
 
 @app.get("/")
-def index():
+def get_index():
     return jsonify(app_status = "running")
+
+@app.post("/ocr/image_to_text")
+def post_index():
+    image = request.files['file']
+    prediction_groups = pipeline.recognize([ image ])
+    predicted_image = prediction_groups[0]
+
+    recognised_text = ""
+
+    for text, box in predicted_image:
+        recognised_text = recognised_text + " " + text
+
+    return jsonify(text = recognised_text)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.getenv('PORT', 80)), threaded=True)
